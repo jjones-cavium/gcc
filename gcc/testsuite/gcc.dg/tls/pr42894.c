e9cd679ca84d49fba4af1c79523ebc9cf1656e49
Andrew Pinski <apinski@cavium.com>
2012-10-14 23:57:46 -0700

    2012-10-14  Andrew Pinski  <apinski@cavium.com>
    	* testsuite/gcc.dg/tls/pr42894.c: Don't add any options if
    	this is arm_hf_eabi.

--- gcc/testsuite/gcc.dg/tls/pr42894.c
+++ gcc/testsuite/gcc.dg/tls/pr42894.c
@@ -1,6 +1,6 @@
 /* PR target/42894 */
 /* { dg-do compile } */
-/* { dg-options "-march=armv5te -mthumb" { target arm*-*-* } } */
+/* { dg-options "-march=armv5te -mthumb" { target { arm*-*-* && { ! arm_hf_eabi } } } } */
 /* { dg-require-effective-target tls } */
 
 extern __thread int t;
