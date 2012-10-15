dac1cb1773f3dad0d3ec0bcb3e495ddc0f4a2959
Andrew Pinski <apinski@cavium.com>
2012-10-15 00:01:21 -0700

    2012-10-15  Andrew Pinski  <apinski@cavium.com>
    	* testsuite/gcc.target/arm/thumb-ltu.c: Also skip for
    	arm_hf_eabi targets.

--- gcc/testsuite/gcc.target/arm/thumb-ltu.c
+++ gcc/testsuite/gcc.target/arm/thumb-ltu.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-skip-if "incompatible options" { arm*-*-* } { "-march=*" } { "-march=armv6" "-march=armv6j" "-march=armv6z" } } */
 /* { dg-options "-mcpu=arm1136jf-s -mthumb -O2" } */
+/* { dg-skip-if "not compatiable with hard-float abi" { { arm*-*-* } && arm_hf_eabi } { "*" } { "" } } */
 
 void f(unsigned a, unsigned b, unsigned c, unsigned d)
 {
