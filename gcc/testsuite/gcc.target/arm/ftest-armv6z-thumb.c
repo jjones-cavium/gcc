08d5cebd688f1717beb3ff4d8401e52228424535
Naveen H.S <Naveen.Hurugalawadi@caviumnetworks.com>
2013-06-21 11:05:25 +0530

    ARM: Skip Thumb related hard float testcases
    2013-06-21  Naveen H.S  <Naveen.Hurugalawadi@caviumnetworks.com>
    
    	* gcc.target/arm/ftest-armv4t-thumb.c: Skip for soft-float Thumb.
    	* gcc.target/arm/ftest-armv5t-thumb.c: Likewise.
    	* gcc.target/arm/ftest-armv5te-thumb.c: Likewise.
    	* gcc.target/arm/ftest-armv6-thumb.c: Likewise.
    	* gcc.target/arm/ftest-armv6k-thumb.c: Likewise.
    	* gcc.target/arm/ftest-armv6z-thumb.c: Likewise.

--- gcc/testsuite/gcc.target/arm/ftest-armv6z-thumb.c
+++ gcc/testsuite/gcc.target/arm/ftest-armv6z-thumb.c
@@ -1,6 +1,7 @@
 /* { dg-do run } */
 /* { dg-require-effective-target arm_eabi } */
 /* { dg-require-effective-target arm_arch_v6z_multilib } */
+/* { dg-skip-if "not compatiable with hard-float abi" { { arm*-*-* } && arm_hf_eabi } { "*" } { "" } } */
 /* { dg-options "-mthumb" } */
 /* { dg-add-options arm_arch_v6z } */
 
