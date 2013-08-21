40a9c9e5516600ca5baef38c2cdd7b3a6f21e5d1
Manjunath S Matti <mmatti@caviumnetworks.com>
2013-08-21 18:51:56 +0530

    [libsanitizer]: Added new testcases to test thread sanitizer.
    2013-08-21  Manjunath Matti  <mmatti@caviumnetworks.com>
    
            gcc/testsuite
            * lib/tsan-dg.exp: New file.
            * gcc.dg/tsan/tsan.exp: New file.
            * c-c++-common/tsan/free_race.c: New testcase.
            * c-c++-common/tsan/free_race2.c: New testcase.
            * c-c++-common/tsan/race_on_barrier.c: New testcase.
            * c-c++-common/tsan/race_on_barrier2.c: New testcase.
            * c-c++-common/tsan/simple_race.c: New testcase.
            * c-c++-common/tsan/simple_stack.c: New testcase.

---
+++ gcc/testsuite/c-c++-common/tsan/free_race2.c
@@ -0,0 +1,30 @@
+
+/* { dg-do run { target { *-*-linux* } } } */
+/* { dg-shouldfail "tsan" } */
+
+#include <stdlib.h>
+
+void __attribute__((noinline)) foo(int *mem) {
+  free(mem);
+}
+
+void __attribute__((noinline)) bar(int *mem) {
+  mem[0] = 42;
+}
+
+int main() {
+  int *mem = (int*)malloc(100);
+  foo(mem);
+  bar(mem);
+  return 0;
+}
+
+/* { dg-output "WARNING: ThreadSanitizer: heap-use-after-free.*" } */
+
+// CHECK:   Write of size 4 at {{.*}} by main thread:
+// CHECK:     #0 bar
+// CHECK:     #1 main
+// CHECK:   Previous write of size 8 at {{.*}} by main thread:
+// CHECK:     #0 free
+// CHECK:     #{{1|2}} foo
+// CHECK:     #{{2|3}} main
---
+++ gcc/testsuite/c-c++-common/tsan/simple_race.c
@@ -0,0 +1,28 @@
+
+/* { dg-do run { target { *-*-linux* } } } */
+/* { dg-shouldfail "tsan" } */
+
+#include <pthread.h>
+#include <stdio.h>
+
+int Global;
+
+void *Thread1(void *x) {
+  Global++;
+  return NULL;
+}
+
+void *Thread2(void *x) {
+  Global--;
+  return NULL;
+}
+
+int main() {
+  pthread_t t[2];
+  pthread_create(&t[0], NULL, Thread1, NULL);
+  pthread_create(&t[1], NULL, Thread2, NULL);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+}
+
+/* { dg-output "WARNING: ThreadSanitizer: data race.*" } */
