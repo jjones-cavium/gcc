a3a46ade8e58ef55c07564e3c0f6d4c1a02a6790
Andrew Pinski <apinski@cavium.com>
2014-11-25 23:18:02 -0800

    Temp fix for bug #13282.  Disable merge bitfields.

--- gcc/opts.c
+++ gcc/opts.c
@@ -511,7 +511,9 @@ static const struct default_options default_options_table[] =
     { OPT_LEVELS_2_PLUS, OPT_fisolate_erroneous_paths_dereference, NULL, 1 },
     { OPT_LEVELS_2_PLUS, OPT_fmerge_const_bfstores, NULL, 1 },
     { OPT_LEVELS_2_PLUS, OPT_fuse_caller_save, NULL, 1 },
+#if 0
     { OPT_LEVELS_2_PLUS, OPT_fmerge_bitfields, NULL, 1 },
+#endif
     { OPT_LEVELS_2_PLUS, OPT_flra_remat, NULL, 1 },
 
     /* -O3 optimizations.  */
